79046bc274d7188e5ec6025db5925b4b69c9ec9d
Leonardo Justo <luanrodriguescosta11@gmail.com>
2022-06-09 17:29:54 -0300

    v2

--- app.py
+++ app.py
@@ -8,12 +8,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html', nome='Fulano')
+  return render_template('index.html', nome='Analise de Seguro')
 
 @app.route('/predicao', methods=['POST'])
 def predicao():
-  comentario = request.form['comentario']
-  predicao = model.predict([comentario])
+
+  gp = float(request.form['gp'])
+  pts = float(request.form['pts'])
+  fg = float(request.form['fg'])
+  ft = float(request.form['ft'])
+  reb = float(request.form['reb'])
+  ast = float(request.form['ast'])
+  stl = float(request.form['stl'])
+  blk = float(request.form['blk'])
+  tov = float(request.form['tov'])
+
+  predicao = model.predict([[gp, pts, fg, ft, reb, ast, stl, blk, tov]])
   return render_template('resposta.html', predicao=predicao[0])
 
 app.run(debug=True)
