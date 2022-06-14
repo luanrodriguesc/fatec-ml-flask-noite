a92497afe38f63ec8392c65ab285ec1f150c6f2d
luanrodriguesc <105249810+luanrodriguesc@users.noreply.github.com>
2022-06-13 22:55:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html', nome='Analise de Seguro')
+  return render_template('index.html', nome='Análise de Seguro')
 
 @app.route('/predicao', methods=['POST'])
 def predicao():
 
-  gp = float(request.form['gp'])
-  pts = float(request.form['pts'])
-  fg = float(request.form['fg'])
-  ft = float(request.form['ft'])
-  reb = float(request.form['reb'])
-  ast = float(request.form['ast'])
-  stl = float(request.form['stl'])
-  blk = float(request.form['blk'])
-  tov = float(request.form['tov'])
+  AGE = request.form['AGE']
+  GENDER = request.form['GENDER']
+  DRIVING_EXPERIENCE = request.form['DRIVING_EXPERIENCE']
+  INCOME = request.form['INCOME']
+  VEHICLE_YEAR = request.form['VEHICLE_YEAR']
 
-  predicao = model.predict([[gp, pts, fg, ft, reb, ast, stl, blk, tov]])
+  array=[[str(AGE), str(GENDER), str(DRIVING_EXPERIENCE), str(INCOME), str(VEHICLE_YEAR)]]
+
+  predicao = model.predict(array)
+
   return render_template('resposta.html', predicao=predicao[0])
 
 app.run(debug=True)
